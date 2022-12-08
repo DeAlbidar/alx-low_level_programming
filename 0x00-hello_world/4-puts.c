440ab48e7723802a05cd663b21342b802f669bd7
DeAlbidar <albidarebenezernarh@gmail.com>
2022-12-08 09:19:40 +0000

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+# include <stdio.h>
+
+/**
+ * main - Print in C
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
