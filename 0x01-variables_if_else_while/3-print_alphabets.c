41f9a7a54b1b84b784f2b61958401f7f08362ca7
DeAlbidar <albidarebenezernarh@gmail.com>
2022-12-09 14:01:56 +0000

    alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i;
+    for(i='a'; i<='z'; i++)
+        putchar(i);
+        
+  	for(i='A'; i<='Z'; i++)
+        putchar(i);
+        
+  	putchar('\n');
+    return 0;
+}
