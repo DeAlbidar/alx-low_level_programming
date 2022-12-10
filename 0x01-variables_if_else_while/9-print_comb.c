55b690fc7f09bd3c61f242353d99569d4ec356de
DeAlbidar <albidarebenezernarh@gmail.com>
2022-12-10 15:22:03 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,12 +9,13 @@ int main(void)
 int i;
 for (i = 0; i < 10; i++){
 	putchar((i % 10) + '0');
-	if (i == 9){
-		continue;
-		putchar(',');
-		putchar(' ');
-	}
+    if (i == 9){
+      continue;
+    }
+    putchar(',');
+    putchar(' ');
 }
 putchar('\n');
 return (0);
 }
+
