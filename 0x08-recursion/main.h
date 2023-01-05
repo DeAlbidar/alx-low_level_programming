46bb8fa2e9dacd4d92df23887a45ef189fb76ba3
DeAlbidar <albidarebenezernarh@gmail.com>
2023-01-05 09:50:18 +0000

    Main

---
+++ 0x08-recursion/main.h
@@ -0,0 +1,22 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+/**
+ * file: main.h
+ * Auth: Albidar
+ * Desc: Header file containing prototpes for all functions
+ *	used in the 0x08-recursion directory.
+ */
+
+int _putchar(char c);
+void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+int factorial(int n);
+int _pow_recursion(int x, int y);
+int _sqrt_recursion(int n);
+int is_prime_number(int n);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
+
+#endif
